f525783a18503e0842aba711442a1d9e6f716ed7
root <root@DESKTOP-MVMOHE0.localdomain>
2025-07-04 20:06:42 +0900

    リスタートまで

--- main.rb
+++ main.rb
@@ -14,26 +14,37 @@ class Worm < Circle
   end
 end
 
+class GameOver
+  attr_accessor :title, :message
+
+  def initialize
+    self.title = Text.new("game over", size: 50, x: 300, y: 100)
+    self.message = Text.new("press key 'R' to restart", size: 30, x: 250, y: 300)
+    self.title.remove
+    self.message.remove
+  end
+end
+
 set width: 800, height: 600
 
-worm_size = 20
+worm_size = 10
 worms = []
 head = 12
 tail = 10
 tick = 0
-speed = 1
-logs = (worm_size * tail).times.map { |i| [Window.width / 2 - i * speed, Window.height / 2 + i * speed] }
+speed = 2
+p logs = (worm_size * speed).times.map { |i| [Window.width / 2 - i , Window.height / 2 + i] }
+p logs.size
 status = :ready
+game_over = GameOver.new
 
-20.times do |n|
+worm_size.times do |n|
   worm = Worm.new
-  worm.x = logs[n * tail].first
-  worm.y = logs[n * tail].last
+  worm.x = logs[n * speed].first
+  worm.y = logs[n * speed].last
   worm.radius = head if n == 0
   worms << worm
 end
-
-#p worms
 
 update do
   worms.each_with_index do |worm, i|
@@ -55,12 +66,13 @@ update do
         logs.unshift([worm.x, worm.y])
         logs.pop
       else
-        worm.x = logs[i * tail].first
-        worm.y = logs[i * tail].last
+        worm.x = logs[i * speed].first
+        worm.y = logs[i * speed].last
       end
-    when :game_over #ここのフラグをもう一段階増やす。messageの変数をグローバルに
-      message = Text.new("game over")
-      status = :ready
+    when :game_over
+      game_over.title.add
+      game_over.message.add
+      status = :stop
     end
 
     if i == 0
@@ -77,12 +89,11 @@ update do
         status = :game_over
       end
       #自分に当たる判定
-      worms[2, 18].each do |w|
+      worms[3, worm_size - 3].each do |w|
         if w.contains?(worm.x, worm.y)
           status = :game_over
         end
       end
-
     end
   end
   tick += 1
@@ -91,41 +102,57 @@ end
 on :key_down do |event|
   key = event.key
   worm = worms.first
-  if worm.x_flug && worm.y_flug
-    case key
-    when "right"
-      worm.x_flug = false
-      tick = 0
-    when "left"
-      worm.y_flug = false
-      tick = 0
+  case status
+  when :ready
+    if worm.x_flug && worm.y_flug
+      case key
+      when "right"
+        worm.x_flug = false
+        tick = 0
+      when "left"
+        worm.y_flug = false
+        tick = 0
+      end
+    elsif worm.x_flug && !worm.y_flug
+      case key
+      when "right"
+        worm.y_flug = true
+        tick = 0
+      when "left"
+        worm.x_flug = false
+        tick = 0
+      end
+    elsif !worm.x_flug && worm.y_flug
+      case key
+      when "right"
+        worm.y_flug = false
+        tick = 0
+      when "left"
+        worm.x_flug = true
+        tick = 0
+      end
+    elsif !worm.x_flug && !worm.y_flug
+      case key
+      when "right"
+        worm.x_flug = true
+        tick = 0
+      when "left"
+        worm.y_flug = true
+        tick = 0
+      end
     end
-  elsif worm.x_flug && !worm.y_flug
-    case key
-    when "right"
-      worm.y_flug = true
-      tick = 0
-    when "left"
-      worm.x_flug = false
-      tick = 0
-    end
-  elsif !worm.x_flug && worm.y_flug
-    case key
-    when "right"
-      worm.y_flug = false
-      tick = 0
-    when "left"
-      worm.x_flug = true
-      tick = 0
-    end
-  elsif !worm.x_flug && !worm.y_flug
-    case key
-    when "right"
-      worm.x_flug = true
-      tick = 0
-    when "left"
-      worm.y_flug = true
-      tick = 0
+  when :stop
+    if key == "r"
+      game_over.title.remove
+      game_over.message.remove
+      logs = (worm_size * speed).times.map { |i| [Window.width / 2 - i , Window.height / 2 + i] }
+      worms.each_with_index do |worm, n|
+        worm.x = logs[n * speed].first
+        worm.y = logs[n * speed].last
+        worm.x_flug = true
+        worm.y_flug = false
+      end
+      status = :ready
     end
   end
 end
