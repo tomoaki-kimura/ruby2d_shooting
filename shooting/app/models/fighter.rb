f4fdb35a0678250fa01bc34d65fd5a9ced9e11d7
root <root@DESKTOP-MVMOHE0.localdomain>
2025-12-05 19:53:50 +0900

    画面リミッター実装

--- shooting/app/models/fighter.rb
+++ shooting/app/models/fighter.rb
@@ -14,5 +14,39 @@ class Fighter < Rectangle
     self.x -= self.speed if key == "a"
     self.y += self.speed if key == "s"
     self.y -= self.speed if key == "w"
+    window_limit
+  end
+
+  private
+
+  def window_limit
+    top_limit
+    bottom_limit
+    left_limit
+    right_limit
+  end
+
+  def top_limit
+    if self.y < 0
+      self.y = 0
+    end
+  end
+
+  def bottom_limit
+    if self.y > Window.height - self.height
+      self.y = Window.height - self.height
+    end
+  end
+
+  def left_limit
+    if self.x < 0
+      self.x = 0
+    end
+  end
+
+  def right_limit
+    if self.x > Window.width - self.width
+      self.x = Window.width - self.width
+    end
   end
 end
