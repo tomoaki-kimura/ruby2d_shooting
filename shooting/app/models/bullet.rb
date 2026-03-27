d4691647ba5fe8e9b4ae46734baeef88a7635046
root <root@DESKTOP-MVMOHE0.localdomain>
2026-03-27 19:54:34 +0900

    Bulletパワーアップ完成

--- shooting/app/models/bullet.rb
+++ shooting/app/models/bullet.rb
@@ -1,23 +1,31 @@
 class Bullet < Circle
-  attr_accessor :speed
+  ANGLE_OFFSET = [0, -15, 15, -30, 30]
+  attr_accessor :speed, :angle
 
   def initialize
     super(
       radius: 3
     )
     self.speed = 15
+    self.angle = 0
   end
 
   def self.set(bullets, fighter)
-    bullet = self.new
-    bullet.x = fighter.x + (fighter.width / 2)
-    bullet.y = fighter.y
-    bullets << bullet
+    bullet_size = fighter.level * 2 - 1
+    bullet_size.times do |i|
+      bullet = self.new
+      bullet.x = fighter.x + (fighter.width / 2)
+      bullet.y = fighter.y
+      bullet.angle = ANGLE_OFFSET[i]
+      bullets << bullet
+    end
   end
 
   def self.move(bullets)
     bullets.each do |bullet|
-      bullet.y -= bullet.speed
+      r = (bullet.angle + 90) * PI / 180
+      bullet.x += cos(r) * bullet.speed
+      bullet.y -= sin(r) * bullet.speed
     end
   end
 end
